bac833ca3a7abeba6f7a87f5c4a5353040cac1d4
Luk4strikee <marianne.vieira.santos@escola.pr.gov.br>
2024-06-27 17:41:32 -0300

    cria bolinha 27/06/2024

--- pingpong.js
+++ pingpong.js
@@ -1,3 +1,7 @@
+//aqui eu vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
 function setup(){
     //aqui vou criar minh "mesa"
     createCanvas(600,400);
@@ -6,4 +10,11 @@ function setup(){
 function draw(){
     //aqui vou por a cor da "mesa"
     background(127,255,212);
+    //chamando a função 
+    criaBolinha(xBolinha,yBolinha,tamBolinha);
+}
+
+//função bolinha
+function criaBolinha(xBolinha,yBolinha,tamBolinha){
+    circle (xBolinha,yBolinha,tamBolinha);
 }
